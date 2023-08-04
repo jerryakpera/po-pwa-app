922a8076f00e3cf0360a5fc6e6df504de5e3ef62
Jeremiah Akpera <jerryakpera@gmail.com>
2023-08-04 20:28:02 +0100

    Filter exercises

--- src/components/ExercisesTable.vue
+++ src/components/ExercisesTable.vue
@@ -4,9 +4,9 @@
     flat
     square
     hide-header
+    :rows="rows"
     row-key="_id"
     :filter="filter"
-    :rows="exercises"
     :columns="columns"
     :loading="loading"
     selection="multiple"
@@ -15,19 +15,62 @@
     :pagination="{ rowsPerPage: 10 }"
   >
     <template v-slot:top>
-      <q-input
-        dense
-        outlined
-        clearable
-        debounce="800"
-        v-model="filter"
-        class="full-width"
-        placeholder="Search exercises by name or body part"
-      >
-        <template v-slot:append>
-          <q-icon name="search" />
-        </template>
-      </q-input>
+      <q-card flat bordered class="full-width">
+        <q-expansion-item
+          dense
+          expand-separator
+          class="full-width"
+          label="Filter exercises"
+        >
+          <q-card-section class="q-pa-xs column q-col-gutter-xs">
+            <q-select
+              dense
+              multiple
+              outlined
+              use-chips
+              color="white"
+              label="Body part"
+              class="full-width"
+              :options="bodyParts"
+              v-model="bodyPartsFilter"
+            >
+              <template #prepend>
+                <POIcon icon="ic:baseline-filter-alt" />
+              </template>
+            </q-select>
+
+            <q-select
+              dense
+              multiple
+              outlined
+              use-chips
+              color="white"
+              label="Equipment"
+              class="full-width"
+              :options="equipments"
+              v-model="equipmentsFilter"
+            >
+              <template #prepend>
+                <POIcon icon="ic:baseline-filter-alt" />
+              </template>
+            </q-select>
+
+            <q-input
+              dense
+              outlined
+              clearable
+              debounce="800"
+              v-model="filter"
+              class="full-width"
+              placeholder="Search exercises"
+            >
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </q-card-section>
+        </q-expansion-item>
+      </q-card>
     </template>
 
     <template v-slot:item="props">
@@ -39,35 +82,65 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import _ from "lodash";
+import { computed, ref } from "vue";
 import { ExerciseCard } from "./exercises";
 
 const props = defineProps(["exercises"]);
 
 const filter = ref("");
 const loading = ref(false);
+const bodyPartsFilter = ref([]);
+const equipmentsFilter = ref([]);
 
 const filterFn = (rows, terms) => {
   const pins = terms?.toLowerCase().trim().split(" ");
   const trimmedPins = pins.map((pin) => pin.trim());
 
   return rows.filter((exercise) => {
-    const { name, target, bodyPart, equipment } = exercise;
-    const hayStack = `${name.toLowerCase()} ${target.toLowerCase()} ${bodyPart.toLowerCase()} ${equipment.toLowerCase()}`;
+    const { name } = exercise;
 
-    let index = -1;
+    let matches = 0;
     for (let i = 0; i < trimmedPins.length; i++) {
       const pin = trimmedPins[i];
 
-      if (hayStack.includes(pin)) {
-        index = 0;
-        break;
+      if (name.toLowerCase().includes(pin)) {
+        matches += 1;
+        continue;
       }
     }
 
-    if (index === 0) return exercise;
+    if (matches > trimmedPins.length - 1) return exercise;
   });
 };
+
+const rows = computed(() => {
+  if (
+    bodyPartsFilter.value.length === 0 &&
+    equipmentsFilter.value.length === 0
+  ) {
+    return [...props.exercises];
+  }
+
+  const filteredExercises = [...props.exercises].filter((exercise) => {
+    const { target, equipment } = exercise;
+    const bodyPartsArray = [...bodyPartsFilter.value].map((bodyPart) =>
+      bodyPart.toLowerCase()
+    );
+    const equipmentsArray = [...equipmentsFilter.value].map((equipment) =>
+      equipment.toLowerCase()
+    );
+
+    if (
+      bodyPartsArray.includes(target) ||
+      equipmentsArray.includes(equipment)
+    ) {
+      return exercise;
+    }
+  });
+
+  return filteredExercises;
+});
 
 const columns = [
   { name: "name", field: "name" },
@@ -75,6 +148,63 @@ const columns = [
   { name: "bodyPart", field: "bodyPart" },
   { name: "equipment", field: "equipment" },
 ];
+
+const bodyParts = [
+  "abs",
+  "quads",
+  "lats",
+  "calves",
+  "pectorals",
+  "glutes",
+  "hamstrings",
+  "adductors",
+  "triceps",
+  "cardiovascular system",
+  "spine",
+  "upper back",
+  "biceps",
+  "delts",
+  "forearms",
+  "traps",
+  "serratus anterior",
+  "abductors",
+  "levator scapulae",
+]
+  .sort()
+  .map((bodyPart) => _.capitalize(bodyPart));
+
+const equipments = [
+  "body weight",
+  "cable",
+  "leverage machine",
+  "assisted",
+  "medicine ball",
+  "stability ball",
+  "band",
+  "barbell",
+  "rope",
+  "dumbbell",
+  "ez barbell",
+  "sled machine",
+  "upper body ergometer",
+  "kettlebell",
+  "olympic barbell",
+  "weighted",
+  "bosu ball",
+  "resistance band",
+  "roller",
+  "skierg machine",
+  "hammer",
+  "smith machine",
+  "wheel roller",
+  "stationary bike",
+  "tire",
+  "trap bar",
+  "elliptical machine",
+  "stepmill machine",
+]
+  .sort()
+  .map((equipment) => _.capitalize(equipment));
 </script>
 
 <style lang="scss">
